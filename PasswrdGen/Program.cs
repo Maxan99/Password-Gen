cb768007185793c42e88b65a2f57350dcab9ca35
Maksimiliyan Metodiev <maxurt99@gmail.com>
2020-12-23 20:20:31 +0000

    first commit

---
+++ PasswrdGen/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+namespace C_
+{
+    class Program
+    {
+        static string input;
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine();
+            GeneratePasswrds(input);
+            
+        }
+        static void GeneratePasswrds(string input){
+            var passwrdChars = input.ToCharArray();
+            char replaceChr = ' ';
+            char oldChr = ' ';
+            string currPasswrd = "";
+           do{
+                for(int j=0;j<passwrdChars.Length-1; j++){
+                    oldChr = passwrdChars[j];
+                    replaceChr = passwrdChars[j+1];
+                    passwrdChars[j] = replaceChr;
+                    passwrdChars[j+1] = oldChr;
+                    currPasswrd =  PasswrdCompare(passwrdChars);
+                    if(input.Equals(currPasswrd)){
+                        continue;
+                    }
+                    PrintArrayElements(passwrdChars);
+                }
+        } while(!input.Equals(currPasswrd));
+        }
+        static void PrintArrayElements(char[] array){
+        foreach(char element in array){
+            Console.Write(element);
+        }
+        Console.WriteLine();
+    }
+    static string PasswrdCompare(char[] chrArray){
+        string comparatorPass = "";
+        foreach(char chr in chrArray){
+            comparatorPass+=chr;
+        }
+        return comparatorPass;
+    }
+    }
+}
+
